4ea0ad07ff7bfe49142d93331892277fcd7600bd
izzdanial <41255403+izzdanial99@users.noreply.github.com>
2022-01-19 03:55:03 +0800

    Add files via upload

---
+++ Lab2_Q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Lab2_Q2{
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        float a = input.nextFloat();
+        float b = input.nextFloat();
+        float c = input.nextFloat();
+        float average = (a + b + c)/3;
+        System.out.print(average);
+    }
+}
